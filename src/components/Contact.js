5f672a78fe2db58048927e067cd60781c050735a
Joshua George <horheyinc8@gmail.com>
2021-09-05 21:56:58 -0500

    email functional

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { SocialIcon } from "react-social-icons";
+import React from "react";
 import emailjs from "emailjs-com";
 import logo from "../images/olguinImages/logo.jpeg";
 
@@ -11,7 +11,7 @@ function Contact() {
 
     emailjs
       .sendForm(
-        "gmail",
+        "service_o4poyjc",
         "template_pi86pem",
         e.target,
         "user_Y6YdfC0dSMM1EiWEZkHLC"
@@ -44,52 +44,25 @@ function Contact() {
             style={{ height: 80, width: 80 }}
           />
         </div>
-        <h3>We are here for any questions y'all have</h3>
+
         <div className="contact-message ">
           <h4>Give us a call or text us a message!</h4>
           <img className="logo-2" src={logo} alt="logo" />
           <h5>Phone: (832) 953-6712</h5>
         </div>
+
         <div className="form center">
-          <h3>Or you can send us an email</h3>
-          <form onSubmit={sendEmail}>
-            <div className="row pt-5 mx-auto">
-              <div className="col-8 form-group mx-auto">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Subject"
-                  name="subject"
-                />
-              </div>
-              <div className="col-8 form-group pt-2 mx-auto">
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Email"
-                  name="email"
-                />
-              </div>
-              <div className="col-8 form-group pt-2 mx-auto">
-                <textarea
-                  className="form-control"
-                  id=""
-                  cols="30"
-                  rows="8"
-                  placeholder="Your message"
-                  name="message"
-                >
-                  {" "}
-                </textarea>
-              </div>
-              <div className="col-8 pt-3 mx-auto">
-                <input
-                  type="submit"
-                  className="btn btn-info"
-                  value="Send Message"
-                ></input>
-              </div>
-            </div>
+          <h3>Feel free to send us an email</h3>
+          <form className="contact-form" onSubmit={sendEmail}>
+            <label>Name:</label>
+            <input type="text" name="name" />
+            <label>Email:</label>
+            <input type="email" name="email" />
+            <label>Subject:</label>
+            <input type="text" name="subject" />
+            <label>Message:</label>
+            <textarea name="message" />
+            <input className="submit" type="submit" value="Send" />
           </form>
         </div>
       </div>
